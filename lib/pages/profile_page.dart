1dafc12e8c0961023eb0601949a5bbbb5ad970b4
Samsrtsns <sametkati30@gmail.com>
2024-10-21 16:04:56 +0300

    Users and profile detail done

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,15 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:socialapp/components/my_back_button.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  final User? currentUser = FirebaseAuth.instance.currentUser;
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(currentUser!.email)
+        .get();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
-        elevation: 0,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: getUserDetails(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CupertinoActivityIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Text("Error : ${snapshot.error}");
+          } else if (snapshot.hasData) {
+            Map<String, dynamic>? user = snapshot.data!.data();
+
+            return Center(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 24, top: 60),
+                    child: Row(
+                      children: [
+                        MyBackButton(),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.all(25),
+                    child: const Icon(
+                      Icons.person,
+                      size: 64,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    user!['username'],
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    "Email : ${user!['email']}",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w500, fontSize: 14),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Text('No data');
+          }
+        },
       ),
     );
   }
--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:socialapp/components/my_back_button.dart';
+import 'package:socialapp/helper/helper_functions.dart';
 
 class UsersPage extends StatelessWidget {
   const UsersPage({super.key});
@@ -6,10 +10,77 @@ class UsersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Users"),
-        elevation: 0,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Users").snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            displayMessageToUser("Something went wrong", context);
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CupertinoActivityIndicator(),
+            );
+          }
+
+          if (snapshot.data == null) {
+            return const Text("No Data");
+          }
+
+          final users = snapshot.data!.docs;
+
+          return Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 24, top: 60),
+                child: Row(
+                  children: [
+                    MyBackButton(),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: users.length,
+                  padding: const EdgeInsets.only(top: 0, left: 16, right: 16),
+                  itemBuilder: (context, index) {
+                    final user = users[index];
+
+                    return Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white60,
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.4),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: ListTile(
+                            title: Text(user['username']),
+                            subtitle: Text(user['email']),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
